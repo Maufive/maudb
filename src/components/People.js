6826c4113eff578126cc165f2107d7a72a1c97a8
Niklas Albinsson <albinssonniklas@gmail.com>
2018-02-13 16:54:36 +0100

    Hola

--- src/components/People.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-export default class People extends Component {
-  render() {
-    return (
-      <div> People page</div>
-    )
-  }
-}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import './scss/index.css';
+import './scss/index.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
